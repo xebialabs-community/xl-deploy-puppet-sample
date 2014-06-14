fef6c4fe53217d35e3126f0746f7c4389f4f7338
Benoit Moussaud <bmoussaud@xebialabs.com>
2014-06-14 16:58:56 +0200

    integrate the XLD module for jboss

--- puppet/modules/xld-jbossas/manifests/init.pp
+++ puppet/modules/xld-jbossas/manifests/init.pp
@@ -15,6 +15,40 @@ class xld-jbossas {
     username             => "admin",
     password             => "admin",
     url                  => "http://10.0.2.2:4516",
-    encrypted_dictionary => "Environments/Puppet/PuppetModuleDictionary"
+    encrypted_dictionary => "Environments/$environment/PuppetModuleDictionary"
   }
+
+  deployit_directory { "Infrastructure/$environment":
+    server   	  => Deployit["xld-jbossas"],
+  }
+
+  deployit_directory { "Environments/$environment":
+    server   	  => Deployit["xld-jbossas"],
+    require    => Deployit_directory["Infrastructure/$environment"]
+  }
+
+  deployit_container { "Infrastructure/$environment/$hostname":
+    type     	  => "overthere.SshHost",
+    properties	=> {
+      os      => UNIX,
+      address => $ipaddress_eth1,
+      username  => vagrant,
+      password => vagrant,
+      connectionType => INTERACTIVE_SUDO,
+      sudoUsername => jbossas,
+    },
+    server   	 => Deployit["xld-jbossas"],
+    require    => Deployit_directory["Infrastructure/$environment"]
+  }
+  deployit_container { "Infrastructure/$environment/$hostname/jboss-$hostname":
+    type     	=> 'jbossas.ServerV5',
+    properties	=> {
+      home 			  => "/opt",
+      serverName 	=> hiera('jbossas::configuration'),
+    },
+    server   	   => Deployit["xld-jbossas"],
+    require 	   => Deployit_container["Infrastructure/$environment/$hostname"],
+    environments => "Environments/$environment/Demo",
+  }
+
 }
